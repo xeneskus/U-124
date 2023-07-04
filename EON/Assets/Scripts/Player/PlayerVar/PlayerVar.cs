a19b55aa7010dd484c0cf75f1802ced6a8f57e4c
2Esenes <104767595+2Esenes@users.noreply.github.com>
2023-07-04 18:09:57 +0300

    healthar

--- EON/Assets/Scripts/Player/PlayerVar/PlayerVar.cs
+++ EON/Assets/Scripts/Player/PlayerVar/PlayerVar.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerVar : MonoBehaviour
 {
-    public int maxHealth = 100;
-    private int curHealth;
+    public float maxHealth = 100;
+    private float curHealth;
+    public Slider healtSlid;
 
     private void Start()
     {
         curHealth = maxHealth;
     }
-
-    public void TakeDamaga(int dmg)
+    private void Update()
     {
+        print(curHealth);
+        if (Input.GetKeyDown(KeyCode.F)) { TakeDamaga(10); }
+        
+    }
+    public void TakeDamaga(int dmg)
+    {    
         curHealth -= dmg;
+        healtSlid.value = curHealth / 100;
     }
 
 }
